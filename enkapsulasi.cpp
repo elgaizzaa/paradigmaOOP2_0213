63bd921ea10e68195c67019932a2ae6a857b1864
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-28 15:52:44 +0700

    menambahkan class remotlampu

---
+++ enkapsulasi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+    private:
+        string saklarNo[10];
+    public:
+        void setsaklarNo(int i, string value) {
+            saklarNo[i] = value;
+        }
+        string getsaklarNo(int i) {
+            return saklarNo[i];
+        }
+
+};
+
