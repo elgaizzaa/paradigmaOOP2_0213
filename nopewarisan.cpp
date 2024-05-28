f28762242c1516506298226ef56bca5a5fa66bdf
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-28 15:31:27 +0700

    membuat kelas baseclass dengan final

--- nopewarisan.cpp
+++ nopewarisan.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+class baseclass final{
+    public:
+    virtual void perkenalan() {
+        cout << "hallo saya function dari base class";
+        
+    }
+};
+
